db21345fdc011bf681daefe840c799c3cfb42a41
Fraberg <frcs.berger@gmail.com>
2020-12-02 23:14:29 +0100

    time ok if need verification

--- back/index.js
+++ back/index.js
@@ -6,6 +6,16 @@ const params = require('./params.json');
 const chalk = require('chalk');
 // const { GoogleSpreadsheet } = require('google-spreadsheet');
 
+const errors = {
+    "!query": "!query - can't find query param",
+    "!puppeteer": "!puppeteer - can't init puppeteer",
+    "!login": "!login - can't login on LinkedIn",
+    "!feed": "!feed - can't reach /feed",
+    "!profile": "!profile - can't find company profile on linkedin",
+    "!about": "!about - could not perform /about scrapping",
+    "!employees": "!employees - could not perform employees scrapping"
+}
+
 // auto scroll helper
 async function autoScroll(page) {
     await page.evaluate(async () => {
@@ -25,19 +35,36 @@ async function autoScroll(page) {
     });
 }
 
-// log helper
-async function printerror(error) {
-    console.error("error:", error);
-    process.exit(1);
-}
-
 // wait helper
 const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
+// run exit
+const run_exit = (status) => {
+    console.log(chalk.yellow("exit", status));
+    process.exit(status);
+}
+
+// log helper
+async function printerror(error, debug, can_exit) {
+    if (debug) { 
+        console.log({"error": error}); await wait(50000);
+    }
+    if (!can_exit) {
+        await wait(50000);
+    }
+    return can_exit;
+}
+
 // main function
 exports.scrapper = async (req, res) => {
-    // debug:
+
+    /* -------------------------------- */
+    // debug
+    /* -------------------------------- */
+    let DEBUG;
+    DEBUG = true;
     // res.status(200).send(JSON.stringify({"req": req.query}));
+    /* -------------------------------- */
 
     /* -------------------------------- */
     // design res object
@@ -76,29 +103,27 @@ exports.scrapper = async (req, res) => {
     // check if linkedin_target exists in query
     /* -------------------------------- */
     if (!req.query.linkedin_target) {
-        let error = { "error": "you must specify linkedin url of some company profile" };
-        res.status(422).send(error);
-        printerror(error);
+        res.status(422).send({"error": errors["!query"]});
+        if (await printerror(errors["!query"], DEBUG, true)) { run_exit(0);; };
     }
     console.log(chalk.cyan("--- cloud function | scrapper ---"));
     /* -------------------------------- */
 
-    /* -------------------------------- */
-    // init Puppeteer
-    /* -------------------------------- */
     try {
+        /* -------------------------------- */
+        // init Puppeteer
+        /* -------------------------------- */
+        let args = [ '--no-sandbox', '--disable-gpu', '--headless' ];
+        if (DEBUG) { args.pop('--headless') };
         let browser = await puppeteer.launch({
             headless: false,
             defaultViewport: null,
-            args: [
-                '--no-sandbox',
-                // '--headless', // comment to debug
-                '--disable-gpu',
-            ]
+            args: args
         });
         let page = await browser.newPage();
-        console.log("page user agent: " + browser.userAgent());
+        // console.log("page user agent: " + browser.userAgent());
         // await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+        /* -------------------------------- */
 
         /* -------------------------------- */
         // login to LinkedIn
@@ -109,21 +134,30 @@ exports.scrapper = async (req, res) => {
             await page.type('#username', params.linkedin.auth.mail, { delay: 30 });
             await page.type('#password', params.linkedin.auth.password, { delay: 30 });
             await Promise.all([await page.click('#app__container > main > div:nth-child(3) > form > div.login__form_action_container > button')]);
-            // await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
             await wait(5000);
             if (page.url() != params.linkedin.urls.feed) {
                 /* -------------------------------- */
                 // verification
                 /* -------------------------------- */
-                await browser.close();
-                await printerror("!=feed: need verification");
+                res.status(422).send({"error": errors["!feed"]});
+                if (await printerror(errors["!feed"], true, false)) { run_exit(0); }
+                await wait(50000);
+                await wait(50000);
+                await wait(50000);
+                await wait(50000);
+                await wait(50000);
+                await wait(50000);
                 /* -------------------------------- */
             }
-            await page.waitFor(params.linkedin.selectors.login_proof_selector);
+            // await page.waitFor(params.linkedin.selectors.login_proof_selector);
+            // await page.$eval(params.linkedin.selectors.login_proof_selector);
+            await wait(1000);
             console.log(chalk.green("successfully logged in"));
-        } catch (error) {
+        } catch {
             await browser.close();
-            await printerror("error, login to LinkedIn:", error);
+            res.status(422).send({"error": errors["!login"]});
+            if (await printerror(errors["!login"], DEBUG, true)) { run_exit(0); };
+            return ;
         }
         /* -------------------------------- */
 
@@ -149,13 +183,17 @@ exports.scrapper = async (req, res) => {
             /* -------------------------------- */
             // check if profile exists
             /* -------------------------------- */
-            if (page.url().includes("/unavailable/")) {
+            await wait(1000);
+            const [company_doesnt_exit] = await page.$x("//p[contains(., 'est pas disponible')]");
+            if (company_doesnt_exit) {
+                console.log(chalk.yellow("profile", req.query.linkedin_target, "does not exist"));
                 await browser.close();
-                res.status(422).send({ "error": "target url is not a company linkedin profile" });
-                await printerror("error, is_not_a_profile_page:", error);
+                res.status(422).send({"error": errors["!profile"]});
+                if (await printerror(errors["!profile"], DEBUG, true)) { run_exit(0); };
+                return ;
             }
+            console.log(chalk.yellow("page is now:", page.url()));
 
-            console.log(chalk.green("page is now:", page.url()));
             const company_selectors = params.linkedin.selectors.company;
 
             // --- banner
@@ -182,14 +220,15 @@ exports.scrapper = async (req, res) => {
 
             // --- about
             // wait untill content is loaded:
-            await page.waitForXPath("//h4[contains(., 'Présentation')]", 5000);
+            // await page.waitForXPath("//h4[contains(., 'Présentation')]", 5000);
+            await wait(1000);
 
             // company.about.description
             const [presentation_h4] = await page.$x("//h4[contains(., 'Présentation')]");
             if (presentation_h4) {
                 let elem = await page.evaluateHandle(el => el.nextElementSibling, presentation_h4);
                 company.about.description = (await (await elem.getProperty("textContent")).jsonValue()).trim();
-                console.log(chalk.green("description:", company.about.description));
+                if (DEBUG) { console.log(chalk.green("description:", company.about.description)); }
             } else {
                 company.about.description = "na";
                 console.log(chalk.redBright("no description"));
@@ -200,7 +239,7 @@ exports.scrapper = async (req, res) => {
             if (site_web_dt) {
                 let elem_site_web = await page.evaluateHandle(el => el.nextElementSibling, site_web_dt);
                 company.about.site_url = (await (await elem_site_web.getProperty("textContent")).jsonValue()).trim();
-                console.log(chalk.green("site_url:", company.about.site_url));
+                if (DEBUG) { console.log(chalk.green("site_url:", company.about.site_url)); }
             } else {
                 company.about.site_url = "na";
                 console.log(chalk.redBright("no site_url"));
@@ -211,7 +250,7 @@ exports.scrapper = async (req, res) => {
             if (sector_dt) {
                 let elem_sector = await page.evaluateHandle(el => el.nextElementSibling, sector_dt);
                 company.about.sector = (await (await elem_sector.getProperty("textContent")).jsonValue()).trim();
-                console.log(chalk.green("sector:", company.about.sector));
+                if (DEBUG) { console.log(chalk.green("sector:", company.about.sector)); }
             } else {
                 company.about.sector = "na";
                 console.log(chalk.redBright("no sector"));
@@ -222,7 +261,7 @@ exports.scrapper = async (req, res) => {
             if (employee_range_dt) {
                 let elem_employee_range = await page.evaluateHandle(el => el.nextElementSibling, employee_range_dt);
                 company.about.employee_range = (await (await elem_employee_range.getProperty("textContent")).jsonValue()).trim();
-                console.log(chalk.green("employee_range:", company.about.employee_range));
+                if (DEBUG) { console.log(chalk.green("employee_range:", company.about.employee_range)); }
                 // company.about.employee_count
                 try {
                     const [employee_count_dt] = await page.$x(`//dd[contains(., '${company.about.employee_range}')]`);
@@ -231,14 +270,14 @@ exports.scrapper = async (req, res) => {
                         let result = (await (await elem_employee_count.getProperty("textContent")).jsonValue()).trim();
                         let i = result.indexOf("sur LinkedIn");
                         company.about.elem_employee_count = result.substring(0, i + 12);
+                        if (DEBUG) { console.log(chalk.green("elem_employee_count:", company.about.elem_employee_count)); }
                     } else {
                         company.about.elem_employee_count = "na";
                         console.log(chalk.redBright("no elem_employee_count"));
                     }
-                    console.log(chalk.green("elem_employee_count:", company.about.elem_employee_count));
                 } catch {
                     company.about.elem_employee_count = "na";
-                    console.log(chalk.redBright("error, no elem_employee_count"));
+                    console.log(chalk.redBright("error, no elem_employee_count:", error));
                 }
             } else {
                 company.about.employee_range = "na";
@@ -250,7 +289,7 @@ exports.scrapper = async (req, res) => {
             if (headquarters_dt) {
                 let elem_headquarters = await page.evaluateHandle(el => el.nextElementSibling, headquarters_dt);
                 company.about.headquarters = (await (await elem_headquarters.getProperty("textContent")).jsonValue()).trim();
-                console.log(chalk.green("headquarters:", company.about.headquarters));
+                if (DEBUG) { console.log(chalk.green("headquarters:", company.about.headquarters)); }
             } else {
                 company.about.headquarters = "na";
                 console.log(chalk.redBright("no headquarters"));
@@ -261,14 +300,16 @@ exports.scrapper = async (req, res) => {
             if (type_dt) {
                 let elem_type = await page.evaluateHandle(el => el.nextElementSibling, type_dt);
                 company.about.type = (await (await elem_type.getProperty("textContent")).jsonValue()).trim();
-                console.log(chalk.green("type:", company.about.type));
+                if (DEBUG) { console.log(chalk.green("type:", company.about.type)); }
             } else {
                 company.about.type = "na";
                 console.log(chalk.redBright("no type"));
             }
-        } catch (error) {
-            console.log(chalk.redBright(await printerror("error, company about:", error)));
+        } catch {
             await browser.close();
+            res.status(422).send({"error": errors["!about"]});
+            if (await printerror(errors["!about"], DEBUG, true)) { run_exit(0); };;
+            return ;
         }
 
         /* -------------------------------- */
@@ -307,12 +348,14 @@ exports.scrapper = async (req, res) => {
                     }
                     let separator = await page.evaluateHandle(el => el.previousElementSibling, elements[i]);
                     employee.name = (await (await separator.getProperty("textContent")).jsonValue()).trim();
-                    console.log(chalk.green("employee.name:", employee.name));
+                    if (DEBUG) { console.log(chalk.green("employee.name:", employee.name)); }
                     company.employees.list.push(employee);
                 }
-            } catch (error) {
-                console.log(chalk.redBright("error, employees:", error));
+            } catch {
                 await browser.close();
+                res.status(422).send({"error": errors["!employees"]});
+                if (await printerror(errors["!employees"], DEBUG, true)) { run_exit(0); };
+                return ;
             }
         }
         /* -------------------------------- */
@@ -322,13 +365,13 @@ exports.scrapper = async (req, res) => {
         /* -------------------------------- */
         // console.log("company:", JSON.stringify(company));
         await browser.close();
+        res.status(200).send(JSON.stringify(company)).end();
+        return ;
         /* -------------------------------- */
-
-    } catch (error) {
+    } catch {
         await browser.close();
-        await printerror("error, init puppeteer:", error);
+        res.status(422).send({"error": errors["!puppeteer"]});
+        if (await printerror(errors["!puppeteer"], DEBUG, true)) { run_exit(0); };
+        return ;
     }
-    /* -------------------------------- */
-    res.status(200).send(JSON.stringify(company)).end();
-    process.exit(0);
 }
