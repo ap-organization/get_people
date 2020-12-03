6163fca95d96a87f1a3d995886dc180e729e219f
Fraberg <frcs.berger@gmail.com>
2020-12-03 01:46:17 +0100

    ok workflow: react, function, APIs, react + gsheet

--- back/index.js
+++ back/index.js
@@ -70,41 +70,57 @@ exports.scrapper = async (req, res) => {
     // design res object
     /* -------------------------------- */
     res.setHeader('Content-Type', 'application/json');
-    let company = {
-        banner: {
-            site_url: "",
-            name: "",
-            nb_followers: "",
-            link_to_employee_list: ""
+    let output = {
+        query: {
+            target_company: "",
+            target_lead: ""
         },
-        // landing: {
-        // },
-        about: {
-            description: "",
-            site_url: "",
-            sector: "",
-            employee_range: "",
-            employee_count: "",
-            headquarters: "",
-            type: ""
-        },
-        // offers: {
-        // },
-        // people: {
-        // },
-        employees: {
-            // hrefs: [],
-            list: []
+        results: {
+            company: {
+                banner: {
+                    site_url: "",
+                    name: "",
+                    nb_followers: "",
+                    link_to_employee_list: ""
+                },
+                about: {
+                    description: "",
+                    site_url: "",
+                    sector: "",
+                    employee_range: "",
+                    employee_count: "",
+                    headquarters: "",
+                    type: ""
+                },
+                employees: {
+                    hrefs: [],
+                    list: []
+                }
+            },
+            lead: {
+                name: "",
+                linkedin_profile: ""
+            }
         }
+        
     };
     /* -------------------------------- */
 
     /* -------------------------------- */
-    // check if linkedin_target exists in query
+    // check if target_company exists in query
+    // check if target_lead exists in query
     /* -------------------------------- */
-    if (!req.query.linkedin_target) {
+    if (req.query.hasOwnProperty('target_company')) {
+        output.query.target_company = req.query.target_company.trim();
+    } else {
+        output.query.target_company = "na";
         res.status(422).send({"error": errors["!query"]});
         if (await printerror(errors["!query"], DEBUG, true)) { run_exit(0);; };
+    }
+    if (req.query.hasOwnProperty('target_lead')) {
+        output.query.target_lead = req.query.target_lead.trim();
+    } else {
+        output.query.target_lead = "na";
     }
     console.log(chalk.cyan("--- cloud function | scrapper ---"));
     /* -------------------------------- */
@@ -122,7 +138,7 @@ exports.scrapper = async (req, res) => {
         });
         let page = await browser.newPage();
         // console.log("page user agent: " + browser.userAgent());
-        // await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+        await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
         /* -------------------------------- */
 
         /* -------------------------------- */
@@ -141,12 +157,7 @@ exports.scrapper = async (req, res) => {
                 /* -------------------------------- */
                 res.status(422).send({"error": errors["!feed"]});
                 if (await printerror(errors["!feed"], true, false)) { run_exit(0); }
-                await wait(50000);
-                await wait(50000);
-                await wait(50000);
-                await wait(50000);
-                await wait(50000);
-                await wait(50000);
+                await wait(120000);
                 /* -------------------------------- */
             }
             // await page.waitFor(params.linkedin.selectors.login_proof_selector);
@@ -177,8 +188,8 @@ exports.scrapper = async (req, res) => {
         // go to company about page and get data
         /* -------------------------------- */
         try {
-            console.log(chalk.yellow("go to:", req.query.linkedin_target));
-            await page.goto(req.query.linkedin_target, { waitUntil: 'domcontentloaded' });
+            console.log(chalk.yellow("go to:", output.query.target_company));
+            await page.goto(output.query.target_company, { waitUntil: 'domcontentloaded' });
 
             /* -------------------------------- */
             // check if profile exists
@@ -186,7 +197,7 @@ exports.scrapper = async (req, res) => {
             await wait(1000);
             const [company_doesnt_exit] = await page.$x("//p[contains(., 'est pas disponible')]");
             if (company_doesnt_exit) {
-                console.log(chalk.yellow("profile", req.query.linkedin_target, "does not exist"));
+                console.log(chalk.yellow("profile", output.query.target_company, "does not exist"));
                 await browser.close();
                 res.status(422).send({"error": errors["!profile"]});
                 if (await printerror(errors["!profile"], DEBUG, true)) { run_exit(0); };
@@ -198,9 +209,9 @@ exports.scrapper = async (req, res) => {
 
             // --- banner
             // linkedIn url
-            company.banner.linkedin_url = params.linkedin.urls.apcapital.landing;
+            output.results.company.banner.linkedin_url = params.linkedin.urls.apcapital.landing;
 
-            // company.banner.link_to_employee_list
+            // output.results.company.banner.link_to_employee_list
             let hrefs = []
             hrefs = await page.evaluate(
                 () => Array.from(
@@ -210,12 +221,12 @@ exports.scrapper = async (req, res) => {
             );
             for (var i = 0; i < hrefs.length; i++) {
                 if (hrefs[i].startsWith("/search/results/people/")) {
-                    company.banner.link_to_employee_list = "https://www.linkedin.com" + hrefs[i];
+                    output.results.company.banner.link_to_employee_list = "https://www.linkedin.com" + hrefs[i];
                     break;
                 }
             }
-            if (company.banner.link_to_employee_list === "") {
-                company.banner.link_to_employee_list = "na";
+            if (output.results.company.banner.link_to_employee_list === "") {
+                output.results.company.banner.link_to_employee_list = "na";
             }
 
             // --- about
@@ -223,86 +234,86 @@ exports.scrapper = async (req, res) => {
             // await page.waitForXPath("//h4[contains(., 'Présentation')]", 5000);
             await wait(1000);
 
-            // company.about.description
+            // output.results.company.about.description
             const [presentation_h4] = await page.$x("//h4[contains(., 'Présentation')]");
             if (presentation_h4) {
                 let elem = await page.evaluateHandle(el => el.nextElementSibling, presentation_h4);
-                company.about.description = (await (await elem.getProperty("textContent")).jsonValue()).trim();
-                if (DEBUG) { console.log(chalk.green("description:", company.about.description)); }
+                output.results.company.about.description = (await (await elem.getProperty("textContent")).jsonValue()).trim();
+                if (DEBUG) { console.log(chalk.green("description:", output.results.company.about.description)); }
             } else {
-                company.about.description = "na";
+                output.results.company.about.description = "na";
                 console.log(chalk.redBright("no description"));
             }
 
-            // company.about.site_url
+            // output.results.company.about.site_url
             const [site_web_dt] = await page.$x("//dt[contains(., 'Site web')]");
             if (site_web_dt) {
                 let elem_site_web = await page.evaluateHandle(el => el.nextElementSibling, site_web_dt);
-                company.about.site_url = (await (await elem_site_web.getProperty("textContent")).jsonValue()).trim();
-                if (DEBUG) { console.log(chalk.green("site_url:", company.about.site_url)); }
+                output.results.company.about.site_url = (await (await elem_site_web.getProperty("textContent")).jsonValue()).trim();
+                if (DEBUG) { console.log(chalk.green("site_url:", output.results.company.about.site_url)); }
             } else {
-                company.about.site_url = "na";
+                output.results.company.about.site_url = "na";
                 console.log(chalk.redBright("no site_url"));
             }
 
-            // company.about.sector
+            // output.results.company.about.sector
             const [sector_dt] = await page.$x("//dt[contains(., 'Secteur')]");
             if (sector_dt) {
                 let elem_sector = await page.evaluateHandle(el => el.nextElementSibling, sector_dt);
-                company.about.sector = (await (await elem_sector.getProperty("textContent")).jsonValue()).trim();
-                if (DEBUG) { console.log(chalk.green("sector:", company.about.sector)); }
+                output.results.company.about.sector = (await (await elem_sector.getProperty("textContent")).jsonValue()).trim();
+                if (DEBUG) { console.log(chalk.green("sector:", output.results.company.about.sector)); }
             } else {
-                company.about.sector = "na";
+                output.results.company.about.sector = "na";
                 console.log(chalk.redBright("no sector"));
             }
 
-            // company.about.employee_range
+            // output.results.company.about.employee_range
             const [employee_range_dt] = await page.$x("//dt[contains(., 'Taille de ')]");
             if (employee_range_dt) {
                 let elem_employee_range = await page.evaluateHandle(el => el.nextElementSibling, employee_range_dt);
-                company.about.employee_range = (await (await elem_employee_range.getProperty("textContent")).jsonValue()).trim();
-                if (DEBUG) { console.log(chalk.green("employee_range:", company.about.employee_range)); }
-                // company.about.employee_count
+                output.results.company.about.employee_range = (await (await elem_employee_range.getProperty("textContent")).jsonValue()).trim();
+                if (DEBUG) { console.log(chalk.green("employee_range:", output.results.company.about.employee_range)); }
+                // output.results.company.about.employee_count
                 try {
-                    const [employee_count_dt] = await page.$x(`//dd[contains(., '${company.about.employee_range}')]`);
+                    const [employee_count_dt] = await page.$x(`//dd[contains(., '${output.results.company.about.employee_range}')]`);
                     if (employee_count_dt) {
                         let elem_employee_count = await page.evaluateHandle(el => el.nextElementSibling, employee_count_dt);
                         let result = (await (await elem_employee_count.getProperty("textContent")).jsonValue()).trim();
                         let i = result.indexOf("sur LinkedIn");
-                        company.about.elem_employee_count = result.substring(0, i + 12);
-                        if (DEBUG) { console.log(chalk.green("elem_employee_count:", company.about.elem_employee_count)); }
+                        output.results.company.about.elem_employee_count = result.substring(0, i + 12);
+                        if (DEBUG) { console.log(chalk.green("elem_employee_count:", output.results.company.about.elem_employee_count)); }
                     } else {
-                        company.about.elem_employee_count = "na";
+                        output.results.company.about.elem_employee_count = "na";
                         console.log(chalk.redBright("no elem_employee_count"));
                     }
                 } catch {
-                    company.about.elem_employee_count = "na";
+                    output.results.company.about.elem_employee_count = "na";
                     console.log(chalk.redBright("error, no elem_employee_count:", error));
                 }
             } else {
-                company.about.employee_range = "na";
+                output.results.company.about.employee_range = "na";
                 console.log(chalk.redBright("no employee_range"));
             }
 
-            // company.about.headquarters
+            // output.results.company.about.headquarters
             const [headquarters_dt] = await page.$x("//dt[contains(., 'Siège social')]");
             if (headquarters_dt) {
                 let elem_headquarters = await page.evaluateHandle(el => el.nextElementSibling, headquarters_dt);
-                company.about.headquarters = (await (await elem_headquarters.getProperty("textContent")).jsonValue()).trim();
-                if (DEBUG) { console.log(chalk.green("headquarters:", company.about.headquarters)); }
+                output.results.company.about.headquarters = (await (await elem_headquarters.getProperty("textContent")).jsonValue()).trim();
+                if (DEBUG) { console.log(chalk.green("headquarters:", output.results.company.about.headquarters)); }
             } else {
-                company.about.headquarters = "na";
+                output.results.company.about.headquarters = "na";
                 console.log(chalk.redBright("no headquarters"));
             }
 
-            // company.about.type
+            // output.results.company.about.type
             const [type_dt] = await page.$x("//dt[contains(., 'Type')]");
             if (type_dt) {
                 let elem_type = await page.evaluateHandle(el => el.nextElementSibling, type_dt);
-                company.about.type = (await (await elem_type.getProperty("textContent")).jsonValue()).trim();
-                if (DEBUG) { console.log(chalk.green("type:", company.about.type)); }
+                output.results.company.about.type = (await (await elem_type.getProperty("textContent")).jsonValue()).trim();
+                if (DEBUG) { console.log(chalk.green("type:", output.results.company.about.type)); }
             } else {
-                company.about.type = "na";
+                output.results.company.about.type = "na";
                 console.log(chalk.redBright("no type"));
             }
         } catch {
@@ -313,11 +324,11 @@ exports.scrapper = async (req, res) => {
         }
 
         /* -------------------------------- */
-        // if link to employees, go to and get names
+        // if link to employees, go to and get names (skip for now)
         /* -------------------------------- */
-        if (company.banner.link_to_employee_list != "na") {
+        if (false === true && output.results.company.banner.link_to_employee_list != "na") {
             try {
-                await page.goto(company.banner.link_to_employee_list, { waitUntil: 'domcontentloaded' });
+                await page.goto(output.results.company.banner.link_to_employee_list, { waitUntil: 'domcontentloaded' });
                 await autoScroll(page);
                 /* -------------------------------- */
                 console.log(chalk.green("page is now:", page.url()));
@@ -330,10 +341,15 @@ exports.scrapper = async (req, res) => {
                         a => a.getAttribute('href')
                     )
                 );
+
                 // get linkedIn url profiles
                 // for (var i = 0; i < hrefs.length; i++) {
-                //     if (hrefs[i].startsWith("/in/") && company.employees.hrefs.indexOf(hrefs[i]) > -1) {
-                //         company.employees.hrefs.push("https://www.linkedin.com" + hrefs[i]);
+                //     if (hrefs[i].startsWith("/in/") && output.results.company.employees.hrefs.indexOf(hrefs[i]) > -1) {
+                //         let complete_url = "https://www.linkedin.com" + hrefs[i];
+                //         output.results.company.employees.hrefs.push(complete_url);
+                //         if (output.params.target_lead != "na" && complete_url === output.params.target_lead) {
+                //             output.results.lead.linkedin_profile = complete_url;
+                //         }
                 //     }
                 // }
 
@@ -349,7 +365,7 @@ exports.scrapper = async (req, res) => {
                     let separator = await page.evaluateHandle(el => el.previousElementSibling, elements[i]);
                     employee.name = (await (await separator.getProperty("textContent")).jsonValue()).trim();
                     if (DEBUG) { console.log(chalk.green("employee.name:", employee.name)); }
-                    company.employees.list.push(employee);
+                    output.results.company.employees.list.push(employee);
                 }
             } catch {
                 await browser.close();
@@ -361,11 +377,43 @@ exports.scrapper = async (req, res) => {
         /* -------------------------------- */
 
         /* -------------------------------- */
+        // get name of target lead
+        /* -------------------------------- */
+        if (output.query.target_lead != "na") {
+            console.log(chalk.yellow("go to:", output.query.target_lead));
+            await page.goto(output.query.target_lead, { waitUntil: 'domcontentloaded' });
+
+            /* -------------------------------- */
+            // check if lead profile exists
+            /* -------------------------------- */
+            await wait(1000);
+            const [lead_doesnt_exit] = await page.$x("//p[contains(., 'est pas disponible')]");
+            if (lead_doesnt_exit) {
+                console.log(chalk.yellow("profile", output.query.target_lead, "does not exist"));
+                await browser.close();
+                res.status(422).send({"error": errors["!profile"]});
+                if (await printerror(errors["!profile"], DEBUG, true)) { run_exit(0); };
+                return ;
+            }
+            console.log(chalk.yellow("page is now:", page.url()));
+            
+            const elements = await page.$$('span.distance-badge');
+            
+            $$('span.distance-badge')[0].parentElement.previousElementSibling.innerText;
+
+            let parent = await page.evaluateHandle(el => el.parentElement, elements[0]);
+            let previous = await page.evaluateHandle(el => el.previousElementSibling, parent);
+            output.results.lead.name = (await (await previous.getProperty("textContent")).jsonValue()).trim();
+            if (DEBUG) { console.log(chalk.green("lead.name:", output.results.lead.name)); }
+        }
+        /* -------------------------------- */
+
+        /* -------------------------------- */
         // close Puppeteer
         /* -------------------------------- */
-        // console.log("company:", JSON.stringify(company));
+        // console.log("output:", JSON.stringify(output));
         await browser.close();
-        res.status(200).send(JSON.stringify(company)).end();
+        res.status(200).send(JSON.stringify(output)).end();
         return ;
         /* -------------------------------- */
     } catch {
